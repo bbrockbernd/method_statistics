efec9f4ae2b2df0b55ce2fe029a032b335dcde24
ioana <savuandreeaioana@yahoo.com>
2020-04-14 14:48:04 +0200

    Show message with link text.

--- src/main/java/core/MarkdownAction.java
+++ src/main/java/core/MarkdownAction.java
@@ -1,9 +1,12 @@
 package core;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.FilenameIndex;
@@ -22,6 +25,9 @@ public class MarkdownAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
+        StringBuffer dlgMsg = new StringBuffer(e.getPresentation().getText());
+        String dlgTitle = e.getPresentation().getDescription();
+
         if (project != null) {
             ArrayList<VirtualFile> mdFiles = (ArrayList<VirtualFile>)
                     FilenameIndex.getAllFilesByExt(project, "md");
@@ -29,13 +35,21 @@ public class MarkdownAction extends AnAction {
                 MarkdownVisitor visitor = new MarkdownVisitor();
                 PsiFile md = PsiManager.getInstance(project).findFile(file);
                 md.accept(visitor);
-                System.out.println(visitor.links);
+
+                for (PsiElement link : visitor.links) {
+                    dlgMsg.append("in class " + file.getName() + " link " + link.getText() + "\n");
+                }
             }
         }
+
+        Messages.showMessageDialog(project, dlgMsg.toString(), dlgTitle,
+                Messages.getInformationIcon());
     }
 
     @Override
     public void update(@NotNull AnActionEvent e) {
-        super.update(e);
+        Project project = e.getProject();
+        e.getPresentation().setEnabledAndVisible(project != null);
+        e.getPresentation().setIcon(AllIcons.Ide.Rating);
     }
 }
